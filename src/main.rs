7d57c0cf529f48048c4a1e9432d55be7593430ed
Benjamin Levy <blevy@protonmail.com>
2022-06-07 01:40:24 -0400

    Upgrade to latest version of serenity

--- src/main.rs
+++ src/main.rs
@@ -16,75 +16,92 @@ use std::iter;
 use std::sync::Arc;
 
 use serenity::{
-    framework::{standard::Delimiter, StandardFramework},
-    model::prelude::*,
+    framework::{
+        standard::{macros::hook, CommandResult, Delimiter},
+        StandardFramework,
+    },
+    model::{gateway::GatewayIntents, prelude::*},
     prelude::*,
+    Result,
 };
 
 /// Bot command prefix
 pub static CMD_PREFIX: &str = "b,";
 
 /// Reply to a message with the debug representation of `dbg`
-fn say_dbg<T: fmt::Debug>(ctx: &Context, msg: &Message, dbg: T) {
-    say(ctx, msg, format!("```{:#?}```", dbg))
+async fn say_dbg<T: fmt::Debug>(ctx: &Context, msg: &Message, dbg: T) {
+    say(ctx, msg, format!("```{:#?}```", dbg)).await
 }
 
 /// If `res` holds an error, reply to a message with the error
-fn say_if_err<T, E: fmt::Debug>(ctx: &Context, msg: &Message, res: Result<T, E>) {
+async fn say_if_err(ctx: &Context, msg: &Message, res: &CommandResult) {
     if let Err(err) = res {
-        say_dbg(ctx, msg, err)
+        say_dbg(ctx, msg, err).await
     }
 }
 
 /// Reply to a message with some content
-pub fn say<T: fmt::Display>(ctx: &Context, msg: &Message, content: T) {
-    if let Err(err) = msg.channel_id.say(&ctx.http, &content) {
+pub async fn say<T: fmt::Display>(ctx: &Context, msg: &Message, content: T) {
+    if let Err(err) = msg.channel_id.say(&ctx.http, &content).await {
         println!("Error saying message '{}': {}", content, err);
     }
 }
 
-/// Set client configuration options
-fn config_client(client: &mut Client) {
-    // Set up command framework
-    client.with_framework(
-        StandardFramework::new()
-            .configure(|c| {
-                c.prefix(CMD_PREFIX)
-                    // Disable argument delimiters
-                    .delimiters::<Delimiter, _>(iter::empty())
-            })
-            .group(&cmd::GENERAL_GROUP)
-            .help(&cmd::HELP)
-            .before(|_ctx, msg, cmd| {
-                println!("Got command '{}' by user '{}'", cmd, msg.author.name);
-                true
-            })
-            .after(|ctx, msg, _cmd, res| say_if_err(ctx, msg, res))
-            .unrecognised_command(|ctx, msg, cmd| {
-                let resp = format!("Command '{}' unrecognized", cmd);
-                say(ctx, msg, resp);
-            })
-            .prefix_only(|ctx, msg| {
-                say(ctx, msg, "Try the `help` sub-command for help.");
-            }),
-    );
+#[hook]
+async fn before_command_hook(_ctx: &Context, msg: &Message, cmd: &str) -> bool {
+    println!("Got command '{}' by user '{}'", cmd, msg.author.name);
+    true
+}
 
-    client_load_state(client);
+#[hook]
+async fn after_command_hook(ctx: &Context, msg: &Message, _cmd: &str, res: CommandResult) {
+    say_if_err(ctx, msg, &res).await
+}
+
+#[hook]
+async fn unrecognized_command_hook(ctx: &Context, msg: &Message, cmd: &str) {
+    let resp = format!("Command '{}' unrecognized", cmd);
+    say(ctx, msg, resp).await
+}
+
+#[hook]
+async fn prefix_only_hook(ctx: &Context, msg: &Message) {
+    say(ctx, msg, "Try the `help` sub-command for help.").await
+}
+
+async fn create_client(token: &str) -> Result<Client> {
+    Client::builder(token, GatewayIntents::all())
+        .event_handler(Handler)
+        .framework(
+            StandardFramework::new()
+                .configure(|c| {
+                    c.prefix(CMD_PREFIX)
+                        // Disable argument delimiters
+                        .delimiters::<Delimiter, _>(iter::empty())
+                })
+                .group(&cmd::GENERAL_GROUP)
+                .help(&cmd::HELP)
+                .before(before_command_hook)
+                .after(after_command_hook)
+                .unrecognised_command(unrecognized_command_hook)
+                .prefix_only(prefix_only_hook),
+        )
+        .await
 }
 
 /// Load saved state from previous run, schedule bedtime alerts accordingly, and
 /// store state in client context
-fn client_load_state(client: &Client) {
+async fn client_load_state(client: &Client) {
     // Load state from previous run
     let state = State::load();
 
     // Store state in context
-    client.data.write().insert::<State>(state);
+    client.data.write().await.insert::<State>(state);
 
     // Schedule bedtime alerts
 
     let http = &client.cache_and_http.http;
-    let mut map = client.data.write();
+    let mut map = client.data.write().await;
     let users = map
         .get_mut::<State>()
         .expect("No state in client")
@@ -93,22 +110,23 @@ fn client_load_state(client: &Client) {
 
     for (&user_id, ref mut user_info) in users {
         let http = Arc::clone(http);
-        user_info.update_sched(http, user_id);
+        user_info.update_sched(http, user_id).await;
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let tok = env::var("DISCORD_TOKEN").expect(
         "Bot token not specified. Please set the `DISCORD_TOKEN` \
          environment variable",
     );
 
     println!("Creating client...");
-    let mut client = serenity::Client::new(&tok, Handler).expect("Couldn't create client");
+    let mut client = create_client(&tok).await.expect("Couldn't create client");
 
-    println!("Configuring client...");
-    config_client(&mut client);
+    println!("Loading previous state...");
+    client_load_state(&client).await;
 
     println!("Starting client...");
-    client.start().expect("Error running client");
+    client.start().await.expect("Error running client");
 }
